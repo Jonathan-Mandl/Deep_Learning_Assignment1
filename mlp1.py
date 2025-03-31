2004c4b3ec5c1358c7152e9438e70303a478c0ca
Jonathan Mandl <118123854+Jonathan-Mandl@users.noreply.github.com>
2025-03-31 11:27:52 +0300

    Add files via upload

---
+++ mlp1.py
@@ -0,0 +1,149 @@
+import numpy as np
+from grad_check import gradient_check
+
+STUDENTS = [
+    {"name": "YOUR NAME", "ID": "YOUR ID NUMBER"},
+    {"name": "YOUR NAME", "ID": "YOUR ID NUMBER"},
+]
+
+def softmax(x):
+    """
+    Compute the softmax vector.
+    x: a n-dim vector (numpy array)
+    returns: an n-dim vector (numpy array) of softmax values
+    """
+    # YOUR CODE HERE
+    # Your code should be fast, so use a vectorized implementation using numpy,
+    # don't use any loops.
+    # With a vectorized implementation, the code should be no more than 2 lines.
+    #
+    # For numeric stability, use the identify you proved in Ex 2 Q1.
+    x = np.exp(x - np.max(x))
+    return x / np.sum(x)
+
+
+def classifier_output(x, params):
+    # YOUR CODE HERE.
+    W,b,U,b_tag = params
+
+    z1 = tanh(np.add(np.dot(W.T,x),b))
+
+    z2 = np.add(np.dot(U.T,z1),b_tag)
+
+    probs = softmax(z2)
+
+    return probs
+
+
+def predict(x, params):
+    """
+    params: a list of the form [W, b, U, b_tag]
+    """
+    return np.argmax(classifier_output(x, params))
+
+
+def tanh(x):
+
+    numerator = np.exp(x) - np.exp(-x)
+    denominator =  np.exp(x) + np.exp(-x)
+
+    return numerator/denominator
+
+
+def tanh_derivative(x):
+    return 1 - np.power(tanh(x), 2)
+
+
+def loss_and_gradients(x, y, params):
+    """
+    params: a list of the form [W, b, U, b_tag]
+
+    returns:
+        loss,[gW, gb, gU, gb_tag]
+
+    loss: scalar
+    gW: matrix, gradients of W
+    gb: vector, gradients of b
+    gU: matrix, gradients of U
+    gb_tag: vector, gradients of b_tag
+    """
+    # YOU CODE HERE
+    W,b,U,b_tag = params
+
+    n_out = U.shape[1]
+
+    z1 = np.dot(W.T, x) + b
+    h = tanh(z1)
+    z2 = np.dot(U.T, h) + b_tag
+    y_hat = softmax(z2)
+
+    y_true = np.zeros(n_out)
+    y_true[y] = 1
+
+    epsilon = 1e-15
+
+    loss = -np.dot(y_true, np.log(np.clip(y_hat, epsilon, 1.0)))
+
+    gl_gz2 = y_hat - y_true
+
+    gU = np.outer(h, gl_gz2) 
+
+    gb_tag =  gl_gz2
+
+    gz1 = np.dot(gl_gz2, U.T) * tanh_derivative(z1)
+
+    x = np.array(x)
+
+    gW = np.outer(x, gz1) 
+
+    gb = gz1
+
+    return loss, [gW, gb, gU, gb_tag]
+
+
+def create_classifier(in_dim, hid_dim, out_dim):
+
+    W = np.random.randn(in_dim, hid_dim) * 0.01 
+    b = np.zeros(hid_dim)
+    U = np.random.randn(hid_dim, out_dim) * 0.01
+    b_tag = np.zeros(out_dim)
+    return [W, b, U, b_tag]
+
+
+if __name__ == "__main__":
+
+    W, b, U, b_tag = create_classifier(3, 6, 2)
+
+    def _loss_and_W_grad(W):
+        global b, U, b_tag
+        loss, grads = loss_and_gradients(np.array([1, 2, 3]), 0, [W, b, U, b_tag])
+        return loss, grads[0]
+
+    def _loss_and_b_grad(b):
+        global W, U, b_tag
+        loss, grads = loss_and_gradients(np.array([1, 2, 3]), 0, [W, b, U, b_tag])
+        return loss, grads[1]
+
+    def _loss_and_U_grad(U):
+        global W, b, b_tag
+        loss, grads = loss_and_gradients(np.array([1, 2, 3]), 0, [W, b, U, b_tag])
+        return loss, grads[2]
+
+    def _loss_and_b_tag_grad(b_tag):
+        global W, b, U
+        loss, grads = loss_and_gradients(np.array([1, 2, 3]), 0, [W, b, U, b_tag])
+        return loss, grads[3]
+
+
+
+    for _ in range(10):
+
+        W = np.random.randn(W.shape[0], W.shape[1])
+        b = np.random.randn(b.shape[0])
+        U = np.random.randn(U.shape[0], U.shape[1])
+        b_tag =np.random.randn(b_tag.shape[0])
+
+        gradient_check(_loss_and_b_grad, b)
+        gradient_check(_loss_and_W_grad, W)
+        gradient_check(_loss_and_U_grad, U)
+        gradient_check(_loss_and_b_tag_grad, b_tag)
---
+++ train_loglin.py
@@ -0,0 +1,121 @@
+import random
+import loglinear as ll
+from utils import *
+import numpy as np
+from collections import Counter
+
+STUDENTS = [
+    {"name": "YOUR NAME", "ID": "YOUR ID NUMBER"},
+    {"name": "YOUR NAME", "ID": "YOUR ID NUMBER"},
+]
+
+
+def feats_to_vec(features):
+    # YOUR CODE HERE.
+    # Should return a numpy vector of features.
+
+    bigram_count = Counter(features)
+
+    x_vec = np.zeros(len(vocab))
+
+    for i,bigram in enumerate(sorted(list(vocab))):
+        x_vec[i] = bigram_count[bigram]
+
+    return x_vec
+
+
+def accuracy_on_dataset(dataset, params):
+    good = bad = 0.0
+    for label, features in dataset:
+        # YOUR CODE HERE
+        # Compute the accuracy (a scalar) of the current parameters
+        # on the dataset.
+        # accuracy is (correct_predictions / all_predictions)
+        
+        label = L2I[label]
+
+        x = feats_to_vec(features)
+
+        pred = ll.predict(x,params)
+
+        if pred == label:
+            good+=1
+        else:
+            bad+=1
+
+    return good / (good + bad)
+
+
+def train_classifier(train_data, dev_data, num_iterations, learning_rate, params):
+    """
+    Create and train a classifier, and return the parameters.
+
+    train_data: a list of (label, feature) pairs.
+    dev_data  : a list of (label, feature) pairs.
+    num_iterations: the maximal number of training iterations.
+    learning_rate: the learning rate to use.
+    params: list of parameters (initial values)
+    """
+    for I in range(num_iterations):
+        cum_loss = 0.0  # total loss in this iteration.
+        random.shuffle(train_data)
+        for label, features in train_data:
+            x = feats_to_vec(features)  # convert features to a vector.
+
+            y = L2I[label]  # convert the label to number if needed.
+
+            loss, grads = ll.loss_and_gradients(x, y, params)
+            cum_loss += loss
+            # YOUR CODE HERE
+            # update the parameters according to the gradients
+            # and the learning rate.
+            W, b = params
+
+            gW, gb = grads
+
+            W = W - learning_rate * gW
+            b = - learning_rate * gb
+
+            params = W, b
+
+        train_loss = cum_loss / len(train_data)
+        train_accuracy = accuracy_on_dataset(train_data, params)
+        dev_accuracy = accuracy_on_dataset(dev_data, params)
+        print(I, train_loss, train_accuracy, dev_accuracy)
+    return params
+
+
+if __name__ == "__main__":
+    # YOUR CODE HERE
+    # write code to load the train and dev sets, set up whatever you need,
+    # and call train_classifier.
+
+    train_data = [(l,text_to_bigrams(t)) for l,t in read_data("data/train")]
+    dev_data   = [(l,text_to_bigrams(t)) for l,t in read_data("data/dev")]
+
+    fc = Counter()
+    for l,feats in train_data:
+        fc.update(feats)
+
+    # 600 most common bigrams in the training set.
+    vocab = set([x for x,c in fc.most_common(600)])
+
+    # label strings to IDs
+    L2I = {l:i for i,l in enumerate(list(sorted(set([l for l,t in train_data]))))}
+    # feature strings (bigrams) to IDs
+    F2I = {f:i for i,f in enumerate(list(sorted(vocab)))}
+
+    in_dim = len(vocab)
+
+    out_dim = len(L2I)
+
+    num_iterations = 10
+    
+    learning_rate = 0.001
+
+    # ...
+
+    params = ll.create_classifier(in_dim, out_dim)
+    trained_params = train_classifier(
+        train_data, dev_data, num_iterations, learning_rate, params
+    )
